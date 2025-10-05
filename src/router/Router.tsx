44c9e41cae091390b2f5b0918710e6f5f28f4b4c
bowoon1216 <happyanne200212@gmail.com>
2025-10-06 00:40:31 +0900

    design: on boardingpage 퍼블리싱

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import { OnboardingPage } from "../pages/OnboardingPage";
 import { HandTrackingPage } from "../pages/HandTrackingPage";
+import { LoginPage } from "../pages/LoginPage";
+import { SignupPage } from "../pages/SignupPage";
 
 const router = createBrowserRouter([
   {
@@ -9,12 +11,20 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/onboarding",
+        path: "/",
         element: <OnboardingPage />,
       },
       {
         path: "/hands-tracker",
         element: <HandTrackingPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/signup",
+        element: <SignupPage />,
       },
     ],
   },
