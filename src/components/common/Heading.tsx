2fbceeb0f63fc992439efb0ea8cfc3951cfd1520
bowoon1216 <happyanne200212@gmail.com>
2025-10-05 23:44:21 +0900

    feat: heading 컴포넌트 제작

---
+++ src/components/common/Heading.tsx
@@ -0,0 +1,18 @@
+interface HeadingProps extends React.HTMLAttributes<HTMLElement> {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export const Heading: React.FC<HeadingProps> = ({
+  children,
+  className = "",
+  ...rest
+}) => {
+  const headingStyle =
+    "flex items-center justify-center w-full text-black text-opacity-50 text-3xl font-light";
+  return (
+    <p className={`${headingStyle} ${className}`} {...rest}>
+      {children}
+    </p>
+  );
+};
--- src/pages/OnboardingPage.tsx
+++ src/pages/OnboardingPage.tsx
@@ -1,4 +1,5 @@
 import { Button } from "../components/common/Botton";
+import { Heading } from "../components/common/Heading";
 import { TextButton } from "../components/common/TextButton";
 
 export const OnboardingPage: React.FC = () => {
@@ -8,6 +9,7 @@ export const OnboardingPage: React.FC = () => {
       <Button>I am Buttton!!</Button>
       <Button disabled>I am Not Buttton!!</Button>
       <TextButton>Text button</TextButton>
+      <Heading>문제업로드</Heading>
     </div>
   );
 };
