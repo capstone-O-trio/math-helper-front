354a72d039dcc341007c2e51d927fd142a53aaa1
bowoon1216 <happyanne200212@gmail.com>
2025-10-06 01:07:11 +0900

    feat: login 페이지 제작

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,40 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "../components/common/Button";
+import { Heading } from "../components/common/Heading";
+import { Input } from "../components/common/Input";
+import { Text } from "../components/common/Text";
+import { TextButton } from "../components/common/TextButton";
+
 export const LoginPage: React.FC = () => {
-  return <div>LoginPage</div>;
+  const naviagate = useNavigate();
+  return (
+    <div className="w-full h-full flex flex-col items-center">
+      <div className="absolute top-4 w-full">
+        <button className="absolute left-8 top-1">이전</button>
+        <Heading>로그인</Heading>
+      </div>
+      <div className="w-[50%] h-full flex flex-col justify-center">
+        <Text>아이디</Text>
+        <Input />
+        <Text className="mt-4">비밀번호</Text>
+        <Input />
+      </div>
+      <div className="absolute bottom-8 right-10 flex flex-col gap-1 w-[12rem]">
+        <Button
+          onClick={() => {
+            naviagate("/login");
+          }}
+        >
+          로그인완료
+        </Button>
+        <TextButton
+          onClick={() => {
+            naviagate("/signup");
+          }}
+        >
+          아직 계정이 없나요?
+        </TextButton>
+      </div>
+    </div>
+  );
 };
