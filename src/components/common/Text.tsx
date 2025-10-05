f0dd5b336aee2ae6af17a85c20383903027cd776
bowoon1216 <happyanne200212@gmail.com>
2025-10-06 01:06:58 +0900

    feat: Text 컴포넌트 제작

---
+++ src/components/common/Text.tsx
@@ -0,0 +1,18 @@
+interface TextProps extends React.HTMLAttributes<HTMLElement> {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export const Text: React.FC<TextProps> = ({
+  children,
+  className = "",
+  ...rest
+}) => {
+  const textStyle =
+    "text-black text-opacity-50 text-xl font-light";
+  return (
+    <p className={`${textStyle} ${className}`} {...rest}>
+      {children}
+    </p>
+  );
+};
