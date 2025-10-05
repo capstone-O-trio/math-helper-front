0b3ccbfb58f2eaad1e7bbb4e526f6feb2f9f380b
bowoon1216 <happyanne200212@gmail.com>
2025-10-05 22:40:12 +0900

    feat: hand tracking page 생성

---
+++ src/pages/HandTrackingPage.tsx
@@ -0,0 +1,10 @@
+import HandTracker from "../components/HandTracker";
+
+export const HandTrackingPage: React.FC = () => {
+  return (
+    <div>
+      HandTrackingPage
+      <HandTracker />
+    </div>
+  );
+};
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
-import { OnboardingPage } from "../pages/Onboarding";
+import { OnboardingPage } from "../pages/OnboardingPage";
+import { HandTrackingPage } from "../pages/HandTrackingPage";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
       {
         path: "/onboarding",
         element: <OnboardingPage />,
+      },
+      {
+        path: "/hands-tracker",
+        element: <HandTrackingPage />,
       },
     ],
   },
