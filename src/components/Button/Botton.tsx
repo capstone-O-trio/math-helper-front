8666d01d7d4a4a33953a413c204c691d9936371a
bowoon1216 <happyanne200212@gmail.com>
2025-10-05 19:52:54 +0900

    feat: Button 공통 컴포넌트 제작

--- src/components/Button/Botton.tsx
+++ src/components/Button/Botton.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { ButtonHTMLAttributes } from "react";
 
-export const Button: React.FC = () => {
-  return <button>Click Me</button>;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  className = "",
+  ...rest
+}) => {
+  const buttonStyle =
+    "flex items-center justify-center rounded-3xl bg-green-100 hover:bg-green-300 text-green-900 border border-green-600 py-5 w-full transition-colors duration-300";
+  return (
+    <button className={`${buttonStyle} ${className}`} {...rest}>
+      <text>{children}</text>
+    </button>
+  );
 };
--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -1,3 +1,10 @@
+import { Button } from "../components/Button/Botton";
+
 export const OnboardingPage: React.FC = () => {
-  return <div>Onboarding Page</div>;
+  return (
+    <div>
+      Onboarding Page
+      <Button>I am Buttton!!</Button>
+    </div>
+  );
 };
