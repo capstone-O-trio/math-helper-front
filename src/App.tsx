361b991a8f6cf051f414c290856c9da5ef6d9714
only4wxx <orchidbyw1@gmail.com>
2025-10-05 13:37:46 +0900

    feat: (임시) 첫 번째 화면에 웹캠 띄우기

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HandTracker from './components/HandTracker';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ fontFamily: "system-ui, sans-serif", padding: 16 }}>
+      <h1>Mediapipe + React 손 추적</h1>
+      <p>브라우저에서 카메라 권한을 허용하세요.</p>
+      <HandTracker />
     </div>
   );
-}
+};
 
 export default App;
