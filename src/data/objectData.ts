c4c02ae578216d05348e85376d0ab47bcb5347da
only4wxx <orchidbyw1@gmail.com>
2025-10-10 00:06:57 +0900

    feat: 문제 풀이에 필요한 객체 정보를 담은 array를 반환

--- src/data/objectData.ts
+++ src/data/objectData.ts
@@ -1,6 +1,23 @@
 // 객체 타입
 export type Obj = { id: string; x: number; y: number; src: string };
 
-const objectsInfo: Obj[] = [ // 문제 풀이를 위한 객체
-  { id: 'apple-1', x: 200, y: 150, src: '/사과.png' }, // 예시 하나
-];
+export function getObjectsInfo(
+    probType="addition", 
+    entity: string, 
+    count1: number, 
+    count2: number): Obj[] {
+    
+    const objectsInfo: Obj[] = [ // 문제 풀이를 위한 객체
+        // 처음엔 아무것도 없음
+    ];
+
+    let objImage = '/사과.png'; // 객체로 넣을 이미지
+    if (entity == 'apple') // 현재는 사과 이미지만 가능
+        objImage = '/사과.png';
+
+    objectsInfo.push(
+        { id: 'apple-1', x: 200, y: 150, src: objImage }
+    );
+
+    return objectsInfo
+}
