0eec238f430505e52fd97f5c29b0a3f5751b1bdd
only4wxx <orchidbyw1@gmail.com>
2025-10-09 23:16:56 +0900

    feat: 문제 풀이에 필요한 객체 생성을 위한 파일을 구분

---
+++ src/data/objectData.ts
@@ -0,0 +1,6 @@
+// 객체 타입
+export type Obj = { id: string; x: number; y: number; src: string };
+
+const objectsInfo: Obj[] = [ // 문제 풀이를 위한 객체
+  { id: 'apple-1', x: 200, y: 150, src: '/사과.png' }, // 예시 하나
+];
