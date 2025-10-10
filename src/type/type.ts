d5e3a8d8e29aa94bf9579ddae370ff9ad24e4672
bowoon1216 <happyanne200212@gmail.com>
2025-10-11 08:50:52 +0900

    feat: 정답확인hook 생성

--- src/type/type.ts
+++ src/type/type.ts
@@ -1,2 +1,7 @@
-
-export type probInfoType = { id: number, probType: string; entity: string; count1: number; count2: number };
+export type probInfoType = {
+  id: number;
+  probType: string;
+  entity: string;
+  count1: number;
+  count2: number;
+};
