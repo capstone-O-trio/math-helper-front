fd0a149ca458c81fe0189cd3fd1d2ae42875aa1d
only4wxx <orchidbyw1@gmail.com>
2025-11-05 01:18:18 +0900

    refactor: HandTracker.tsx 파일 여러 구조로 나눠서 세분화

--- src/features/handtracking/HandRenderer.tsx
+++ src/features/handtracking/HandRenderer.tsx
@@ -1,3 +1,39 @@
 /*
     HandRenderer.tsx -> 캔버스, 객체, 드롭존 렌더링
 */
+
+export const HandRenderer = ({ objects, camRatio, canvasRef }: any) => (
+    <>
+        {/* 오버레이 캔버스: 랜드마크/뼈대 등을 그리는 레이어 */}
+        <canvas
+            ref={canvasRef}
+            style={{
+                position: "absolute",
+                inset: 0,
+                width: "100%",
+                height: "100%",
+                pointerEvents: "none",
+                zIndex: 2,
+            }}
+        />
+
+        {/* 객체를 화면에 표시 */}
+        {objects.map(({ id, x, y, src }: any) => (
+            <img
+                key={id}
+                src={src}
+                alt={id}
+                style={{
+                position: "absolute",
+                left: x * camRatio,
+                top: y * camRatio,
+                width: 48,
+                height: 48,
+                transform: "translate(-50%, -50%)",
+                pointerEvents: "none",
+                zIndex: 3,
+                }}
+            />
+        ))}
+    </>
+);
