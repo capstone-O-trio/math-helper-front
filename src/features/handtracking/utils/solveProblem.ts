4f59190d2ccb351816d9f76f20e162913fc844bc
only4wxx <orchidbyw1@gmail.com>
2025-11-06 03:13:21 +0900

    feat: ratio 반영해서 드롭존 위치 확인하게 수정

--- src/features/handtracking/utils/solveProblem.ts
+++ src/features/handtracking/utils/solveProblem.ts
@@ -1,5 +1,10 @@
+/*
+    solveProblem.ts
+*/
+
 // 객체가 드롭존 안에 있는지 확인
-function isInDropZone(
+export function isInDropZone(
+    ratio: number,
     dx: number,
     dy: number,
     dw: number,
@@ -8,14 +13,19 @@ function isInDropZone(
     oy: number,
     isObj: boolean
 ): boolean {
+    const new_dx = dx * ratio;
+    const new_dy = dy * ratio;
+    const new_dw = dw * ratio;
+    const new_dh = dh * ratio;
+    const new_ox = ox * ratio;
+    const new_oy = oy * ratio;
+
     if (
         isObj === true &&
-        ox >= dx &&
-        ox <= dx + dw &&
-        oy >= dy &&
-        oy <= dy + dh
+        new_ox >= new_dx &&
+        new_ox <= new_dx + new_dw &&
+        new_oy >= new_dy &&
+        new_oy <= new_dy + new_dh
     )   return true;
     else return false;
 }
-
-export { isInDropZone }
