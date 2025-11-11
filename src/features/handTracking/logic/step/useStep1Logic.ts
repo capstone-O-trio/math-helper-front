f5ec22d78504b922e38e9a3e9241245321a8aa6f
only4wxx <orchidbyw1@gmail.com>
2025-11-11 18:39:47 +0900

    feat: 사과 빼내기 템플릿 반환할 수 있도록 추가

--- src/features/handTracking/logic/step/useStep1Logic.ts
+++ src/features/handTracking/logic/step/useStep1Logic.ts
@@ -3,8 +3,9 @@
 */
 
 import { useAdditionTemplate } from "../templates/additionTemplate";
+import { useAppletakeoutTemplate } from "../templates/appletakeoutTemplate";
 
-export const useStep1Logic = ({ 
+export const useStep1Logic = ({
     stepRef, mathProbInfo, canvasRef, camRatioRef,
     setStep, setComment, selectAnswer, navigate
 }: any) => {
@@ -12,12 +13,18 @@ export const useStep1Logic = ({
     const addition = useAdditionTemplate({
         mathProbInfo, canvasRef, camRatioRef, setStep, setComment, selectAnswer, navigate
     });
+    const appletakeout = useAppletakeoutTemplate({
+        mathProbInfo, canvasRef, camRatioRef, setStep, setComment, selectAnswer, navigate
+    });
 
     // 필요한 템플릿만 반환 (조건부 반환은 OK)
     if (mathProbInfo.probTemplate === "addition") {
         return addition;
     }
+    else if (mathProbInfo.probTemplate === "appletakeout") {
+        return appletakeout;
+    }
 
     // 다른 템플릿 대비 기본 반환
-    return { objects: [], onResults: () => {} };
+    return { objects: [], onResults: () => { } };
 }
