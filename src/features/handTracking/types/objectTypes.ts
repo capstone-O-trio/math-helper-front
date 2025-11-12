54324a8d2a98e17c7903ce5740a5c4d167448199
gaeul3303 <rkdmfghd@naver.com>
2025-11-13 04:27:40 +0900

    feat: 객체 타입에 rotation 추가

--- src/features/handTracking/types/objectTypes.ts
+++ src/features/handTracking/types/objectTypes.ts
@@ -4,8 +4,13 @@
 
 // 객체 타입
 export type Obj = {
-    id: string; x: number; y: number; src: string,
-    isObj: boolean, // 객체인지
-    value: null | number, // 숫자라면 값이 있음
-    width: number, height: number, // 크기
+    id: string;
+    x: number;
+    y: number;
+    src: string;
+    isObj: boolean; // 객체인지
+    value: null | number; // 숫자라면 값이 있음
+    width: number;
+    height: number; // 크기
+    rotation?: number;
 };
