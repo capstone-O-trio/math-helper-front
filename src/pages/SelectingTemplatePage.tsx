2d93c8a9fe9508351d771297e1536b016406ccf5
bowoon1216 <happyanne200212@gmail.com>
2025-11-17 02:14:07 +0900

    feat: page 초기세팅

---
+++ src/pages/SelectingTemplatePage.tsx
@@ -0,0 +1,10 @@
+import { Button } from "components/common/Button";
+
+export const SelectingTemplatePage = () => {
+  return (
+    <>
+      selecting template page
+      <Button>문제 풀기</Button>
+    </>
+  );
+};
