3fd6af21abb2a37fdbe523207218e705173563e5
bowoon1216 <happyanne200212@gmail.com>
2025-11-17 15:17:50 +0900

    feat: template card component 생성

---
+++ src/components/selectTemplate/TemplateCard.tsx
@@ -0,0 +1,9 @@
+import { TEMPLATE_INFO } from "pages/SelectingTemplatePage";
+
+type TemplatePropsType = {
+  temInfo: TEMPLATE_INFO;
+};
+
+export const TemplateCard = ({ temInfo }: TemplatePropsType) => {
+  return <div>this is card</div>;
+};
--- src/pages/SelectingTemplatePage.tsx
+++ src/pages/SelectingTemplatePage.tsx
@@ -1,10 +1,43 @@
 import { Button } from "components/common/Button";
+import { Text } from "components/common/Text";
+import { TemplateCard } from "components/selectTemplate/TemplateCard";
+
+export type TEMPLATE_INFO = {
+  id: number;
+  templateName: string; //사과로 더해보기
+  templateImage: string; //미리보기 이미지
+  available: boolean; //이용 가능한지아닌지(구현?)
+};
+
+const tem_list: TEMPLATE_INFO[] = [
+  {
+    id: 0,
+    templateName: "사과로 더해보기",
+    templateImage: "미리보기 이미지",
+    available: true,
+  },
+  {
+    id: 1,
+    templateName: "사과 나누기",
+    templateImage: "미리보기 이미지",
+    available: false,
+  },
+  {
+    id: 2,
+    templateName: "저울",
+    templateImage: "미리보기 이미지",
+    available: false,
+  },
+];
 
 export const SelectingTemplatePage = () => {
   return (
-    <>
-      selecting template page
+    <div>
+      <Text>풀이를 선택해봐!</Text>
+      {tem_list.map((template) => {
+        return <TemplateCard temInfo={template} />;
+      })}
       <Button>문제 풀기</Button>
-    </>
+    </div>
   );
 };
