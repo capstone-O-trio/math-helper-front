2b72684197e4afca6c00439e031d6ddfcc2f003b
bowoon1216 <happyanne200212@gmail.com>
2025-11-20 00:18:41 +0900

    feat: map item에 key prop 추가

--- src/pages/SelectingTemplatePage.tsx
+++ src/pages/SelectingTemplatePage.tsx
@@ -56,7 +56,7 @@ export const SelectingTemplatePage = () => {
       >
         {temList.length === 0 && <div>{"가능한 풀이가 없습니다."}</div>}
         {temList.map((template) => (
-          <TemplateCard temInfo={template} mathId={mathProbId} />
+          <TemplateCard key={template.templateId} temInfo={template} mathId={mathProbId} />
         ))}
       </div>
     </div>
