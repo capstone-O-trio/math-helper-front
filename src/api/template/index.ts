1628551c196ca51c04e38f5267e8d318313c9898
bowoon1216 <happyanne200212@gmail.com>
2025-11-20 14:19:11 +0900

    feat: template 별 파라미터 호출 api

--- src/api/template/index.ts
+++ src/api/template/index.ts
@@ -4,12 +4,24 @@ import { ACCESS_TOKEN_KEY } from "utils/keys";
 
 export const getTemplateList = async (typeName: string) => {
   const response = await instance.get<TemplateListResponse>(
-      `/maths/templates?typeName=${typeName}`,
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN_KEY) || ""}`,
-        },
-      }
-    );
-    return response.data;
+    `/maths/templates?typeName=${typeName}`,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN_KEY) || ""}`,
+      },
+    }
+  );
+  return response.data;
+};
+
+export const postTemplateParam = async (mathId: number, templateId: number) => {
+  const response = await instance.post(
+    `/maths/parameters/${mathId}?templateId=${templateId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN_KEY) || ""}`,
+      },
+    }
+  );
+  return response.data
 };
