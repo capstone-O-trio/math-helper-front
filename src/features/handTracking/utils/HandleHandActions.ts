c06babf6a0321ea624b58ea0d0b93ead65484736
only4wxx <orchidbyw1@gmail.com>
2025-11-28 20:44:15 +0900

    delete: 필요 없는 코드 및 주석 삭제

--- src/features/handTracking/utils/HandleHandActions.ts
+++ src/features/handTracking/utils/HandleHandActions.ts
@@ -189,21 +189,7 @@ export const HandleHandActions = (
                 // 원래 버튼을 누르고 있었다가 뗀 경우
                 if (selectedButtonId === "button-next") {
                   // '정답 맞추러 가기 버튼'을 누르다가 뗀 경우
-                  //setStep(2); // 다음 단계로
                   navigate("/hands-tracker/check"); // 정답 맞추기 페이지로 이동
-                }
-                if (selectedButtonId === "button-check-answer") {
-                  // '정답인지 확인하기 버튼'을 누르다가 뗀 경우
-                  // 정답 확인
-                  // if (selectAnswer !== null) {
-                  //     if (selectAnswer === mathProbInfo.answer) {
-                  //         setComment("정답입니다! 짝짝짝!");
-                  //     } else {
-                  //         setComment("오답입니다.. ㅠㅠ");
-                  //     }
-                  // } else {
-                  //     setComment("선택한 답이 없습니다..!");
-                  // }
                 }
                 if (selectedButtonId === "button-back") {
                   // '되돌아가기 버튼'을 누르다가 뗀 경우
